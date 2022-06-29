add29bec9ca226d457b45a9882be8d3504919e91
merlinthemagic <github@martinpetermadsen.com>
2022-06-29 15:38:13 +0100

    aefea

--- APP/Src/Facts/Test.php
+++ APP/Src/Facts/Test.php
@@ -7,95 +7,7 @@ class Test extends Base
 	public function execute()
 	{
 		
-		$raw	= "[admin@rps-initializated] > /system/resource/print
-                   uptime: 22h57m31s
-                  version: 7.3.1 (stable)
-               build-time: Jun/09/2022 08:58:15
-         factory-software: 6.45.9
-              free-memory: 74.6MiB
-             total-memory: 128.0MiB
-                      cpu: ARM
-                cpu-count: 4
-            cpu-frequency: 448MHz
-                 cpu-load: 0%
-           free-hdd-space: 2048.0KiB
-          total-hdd-space: 15.2MiB
-  write-sect-since-reboot: 732
-         write-sect-total: 1635
-               bad-blocks: 0%
-        architecture-name: arm
-               board-name: hAP ac^2
-                 platform: MikroTik
-[admin@rps-initializated] >
-";
-		
-		$cmd		= "/system/resource/print";
-		
-// 		$data		= $raw;
-// 		$strCmd		= $cmd;
-// 		if ($strCmd !== null && trim($strCmd) != "") {
-			
-// 			$lines		= explode("\n", $data);
-// 			$found		= false;
-			
-// 			echo "\n <code><pre> \nClass:  ".__CLASS__." \nMethod:  ".__FUNCTION__. "  \n";
-// 			//var_dump($_SERVER);
-// 			echo "\n 2222 \n";
-// 			//print_r($_GET);
-// 			echo "\n 3333 \n";
-// 			print_r($lines);
-// 			echo "\n ".time()."</pre></code> \n ";
-// 			die("end");
-			
-// 			foreach ($lines as $lKey => $line) {
-				
-// 				$line	= trim($line);
-// 				if ($line != "") {
-					
-// 					//after each 1000 chars the commands encur a terminal break
-// 					$line	= str_replace("\r\n", "", $line);
-// 					//is the line part of the command?
-// 					$cmdPos		= strpos($strCmd, $line);
-// 					//is the command only part of the line
-// 					//e.g. there is more data than just the command
-// 					$linePos    = strpos($line, $strCmd);
-					
-// 					if ($cmdPos !== false || $linePos !== false) {
-// 						//this line holds part or all of the command
-// 						$found		= true;
-// 						if ($cmdPos !== false) {
-// 							$strCmd		= substr($strCmd, ($cmdPos + strlen($line)));
-// 							if (strlen(trim($strCmd)) < 1) {
-// 								//we found all of the command, nothing but whitespace left
-// 								$lines		= array_slice($lines, ($lKey + 1));
-// 								break;
-// 							}
-							
-// 						} elseif ($linePos !== false) {
-							
-// 							//this line holds what remains of the command, the rest is data
-// 							//the rest of the line is data, if it was only whitespace, it would have been caught above
-// 							$lines[$lKey]	= substr($line, ($linePos + strlen($strCmd)));
-// 							$lines			= array_slice($lines, $lKey);
-// 							break;
-// 						}
-						
-// 					} elseif ($found === true) {
-// 						//we had part of the command but lost it before a match could be made
-// 						break;
-// 					}
-// 				}
-// 			}
-// 			$data	= implode("\n", $lines);
-// 		}
-// 		echo "\n <code><pre> \nClass:  ".__CLASS__." \nMethod:  ".__FUNCTION__. "  \n";
-// 		var_dump(chr(47));
-// 		echo "\n 2222 \n";
-// 		//print_r($_GET);
-// 		echo "\n 3333 \n";
-// // 		print_r($_POST);
-// 		echo "\n ".time()."</pre></code> \n ";
-// 		die("end");
+	
 		
 		$rData		= array();
 // 		$toolObj	= \MTM\Mikrotik\Factories::getTools()->getNetInstall();
@@ -116,20 +28,68 @@ class Test extends Base
 
 		$secMacAddr		= "DC:2C:6E:E4:5A:12";
 		$secUsername	= "admin";
-		$secPassword	= "merlin";
+		$secPassword	= "";
 		$secUsername	= \MTM\SSH\Factories::getShells()->getRouterOsTool()->getFormattedUsername($secUsername);
 		$ctrlObj		= \MTM\MacTelnet\Factories::getShells()->passwordAuthentication($secMacAddr, $secUsername, $secPassword, $ctrlObj);
 
-		$cmdStr		= "/ip/dhcp-client/print";
-		$data		= trim($ctrlObj->getCmd($cmdStr)->get());
 		
-		if ($data == "") {
-			$cmdStr		= "/ip/dhcp-client/add interface=ether1 disabled=no add-default-route=yes default-route-distance=250";
-			$ctrlObj->getCmd($cmdStr)->get();
+// 		$username	= "martin_adm";
+// 		$password	= "TtPr0Me1E@";
 		
-			$cmdStr		= "/ip/dhcp-client/print";
-			$data		= trim($ctrlObj->getCmd($cmdStr)->get());
+// 		$cmdStr		= "/user/remove [find where name=\"".$username."\"];";
+// 		$data		= trim($ctrlObj->getCmd($cmdStr)->get());
+		
+// 		$cmdStr		= "/user/add name=\"".$username."\" password=\"".$password."\" group=\"full\";";
+// 		$data		= $ctrlObj->getCmd($cmdStr)->get();
+
+		
+		
+		$cmdStr			= ":put [/system/routerboard/get model];";
+		$modelNbr		= trim($ctrlObj->getCmd($cmdStr)->get());
+		$rData[]		= $modelNbr;
+		
+		
+		$cmdStr			= ":put [/system/license/get software-id];";
+		$license		= trim($ctrlObj->getCmd($cmdStr)->get());
+		$rData[]		= $license;
+		
+		if ($modelNbr === "RBD52G-5HacD2HnD") {
+			$hashTool	= \MTM\Utilities\Factories::getStrings()->getHashing();
+			$devNbr		= $hashTool->getAsInteger(hash("sha256", $license."qefqwegh23hh5hFytdf"), 99999);
+			if ($devNbr < 20000) {
+				$devNbr		= $hashTool->getAsIntegerV2(hash("sha256", $license."qefqwegh23hh5hFytdf"), 99999);
+				if ($devNbr < 20000) {
+					throw new \Exception("Device Nbr too low");
+				}
+			}
+			
+			$identity	= "ap-".$devNbr.".lionstripe.com";
+			
+		} else {
+			throw new \Exception("Not handled for model: '".$modelNbr."'");
 		}
+		$rData[]		= $identity;
+		
+		$cmdStr			= "/tool fetch url=\"https://dac-test-grp.dynamic-data.io/api/v1/Provisioning/Get/RouterOSv7/RpsInitial/8E5ECB0A-B39E-68B1-450E-EEE820667841/\" port=47480 mode=https user=\"".getenv("dd-dac.api.user")."\" password=\"".getenv("dd-dac.api.pass")."\" http-method=get output=file as-value dst-path=flash/RPS/primary.rsc";
+		$iData			= trim($ctrlObj->getCmd($cmdStr)->get());
+		
+
+		$rData[]		= $iData;
+		
+		
+		
+// 		$cmdStr		= "/ip/dhcp-client/print";
+// 		$data		= trim($ctrlObj->getCmd($cmdStr)->get());
+// 		if ($data == "") {
+// 			$cmdStr		= "/ip/dhcp-client/add interface=ether1 disabled=no add-default-route=yes default-route-distance=250";
+// 			$ctrlObj->getCmd($cmdStr)->get();
+		
+// 			$cmdStr		= "/ip/dhcp-client/print";
+// 			$data		= trim($ctrlObj->getCmd($cmdStr)->get());
+// 		}
+		
+		
+		// 		
 		
 // 		$rawData	= $cmdObj->getData();
 // 		$rData[]	= $pData;
@@ -140,11 +100,11 @@ class Test extends Base
 	
 		
 		echo "\n <code><pre> \nClass:  ".__CLASS__." \nMethod:  ".__FUNCTION__. "  \n";
-		var_dump($data);
+// 		var_dump($data);
 		echo "\n 2222 \n";
 // 		print_r($rawData);
 		echo "\n 3333 \n";
-		print_r($data);
+		print_r($rData);
 		echo "\n ".time()."</pre></code> \n ";
 		die("end");
 		
---
+++ APP/Src/Resources/Net-Install/current.rsc
@@ -0,0 +1,110 @@
+
+##begin, tested on ROS v6.49.6 and v7.3.1
+:log info message=("Starting net-install RPS default configuration script v1");
+
+##init will be complete when the executed script sets the global var: rpsInit = true
+##this disables the schedule and stops the script from triggering 
+##default config scripts timeout after running for 240 secs, scheduled jobs do not have this limit
+
+:local ethCount 1; ##minimum loaded ethernet interface count
+:local result ""; ##general return data
+:local counter 0; ##general counter data
+:local isDone false; ##general did a job complete
+
+##make sure the ethernet interfaces are loaded before we start
+:set counter 0;
+:set isDone false;
+:log info message=("Waiting for a minimum of: '".$ethCount."' ethernet interfaces to be ready");
+:while ($isDone = false) do={
+	:set counter [:len [/interface ethernet find]];
+	:if ($counter >= $ethCount) do={
+		:set isDone true;
+		:log info message=("'".$ethCount."' ethernet interfaces ready!");
+	} else={
+		:delay 1s;
+	}
+};
+
+##we rely on ssh/mac-telnet and the annoying renew password prompts are a problem
+:log info message=("Setting identity");
+:set result [/system identity set name="rps-initializated"];
+
+##we create a scheduled task to tackle the init scripts. This way if the script fails
+## e.g. bc there is no internet or power is lost, the device will try again until it succeds
+
+:local scrName "rpsInit"; ##script name
+:local schName "rpsInit"; ##schedule name
+
+##build the script line by line
+:local s [:toarray ""];
+:set ($s->([:len $s])) ":global rpsInitRun;";
+:set ($s->([:len $s])) ":if (\$rpsInitRun != true) do={";
+:set ($s->([:len $s])) ":set rpsInitRun true;";
+:set ($s->([:len $s])) ":global rpsInit;";
+:set ($s->([:len $s])) ":set rpsInit false;";
+:set ($s->([:len $s])) ":local scrJob \"\";";
+:set ($s->([:len $s])) ":local scrNull \"initNull.txt\";";
+:set ($s->([:len $s])) ":local loadFiles [:toarr \"flash/RPS/primary.rsc,flash/RPS/secondary.rsc\"];";
+:set ($s->([:len $s])) ":local res \"\";";
+:set ($s->([:len $s])) ":local isDone false;";
+:set ($s->([:len $s])) ":local counter 0;";
+:set ($s->([:len $s])) ":foreach path in=\$loadFiles do={";
+:set ($s->([:len $s])) ":if (\$isDone = false) do={";
+:set ($s->([:len $s])) ":if ([:len [/file find where name=\$path]] > 0) do={";
+:set ($s->([:len $s])) ":do {";
+:set ($s->([:len $s])) ":log info message=(\"Loading Initialization file: '\".\$path.\"'\");";
+:set ($s->([:len $s])) ":set counter 120;";
+:set ($s->([:len $s])) ":set scrJob [:execute script=(\"/import file-name=\".\$path) file=(\$scrNull)];";
+:set ($s->([:len $s])) ":while (\$counter > 0) do={"; ##start while
+:set ($s->([:len $s])) ":set res [/system script job get \$scrJob type];";
+:set ($s->([:len $s])) ":if ([:typeof \$res] = \"nil\") do={";
+:set ($s->([:len $s])) ":set counter 0;";
+:set ($s->([:len $s])) "} else={";
+:set ($s->([:len $s])) ":set counter (\$counter - 1);";
+:set ($s->([:len $s])) ":delay 1s;";
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) "}"; ##end while
+:set ($s->([:len $s])) ":if (\$rpsInit = true) do={";
+:set ($s->([:len $s])) ":set isDone true;";
+:set ($s->([:len $s])) ":log info (\"RPS init file: '\".\$path.\"' success!\");";
+:set ($s->([:len $s])) "} else={";
+:set ($s->([:len $s])) ":log warning (\"RPS init file: '\".\$path.\"' failed!\");";
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) "} on-error={";
+:set ($s->([:len $s])) ":log error message=(\"RPS init file: '\".\$path.\"' failed in error!\");";
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) "} else={";
+:set ($s->([:len $s])) ":log warning message=(\"RPS init file: '\".\$path.\"' does not exist!\");";
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) ":if (\$rpsInit != true) do={";
+:set ($s->([:len $s])) ":log error message=(\"RPS initialization failed!\");";
+:set ($s->([:len $s])) "} else={";
+:set ($s->([:len $s])) ":log info message=(\"RPS initialization success!\");";
+:set ($s->([:len $s])) (":if ([:len [/system scheduler find where name=\"".$schName."\"]] > 0) do={");
+:set ($s->([:len $s])) ("/system scheduler set [find where name=\"".$schName."\"] disabled=yes");
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) "}";
+:set ($s->([:len $s])) ":set rpsInitRun false;";
+:set ($s->([:len $s])) "} else={";
+:set ($s->([:len $s])) ":log info message=(\"RPS initialization still in progress, skipping!\");";
+:set ($s->([:len $s])) "}";
+
+:local scr "";
+:foreach line in=$s do={
+	:set scr ($scr.$line."\r\n");
+}
+:if ([:len [/system script find where name=$scrName]] < 1) do={
+	/system script add name=$scrName source=$scr dont-require-permissions=no policy=ftp,reboot,read,write,policy,test,password,sniff,sensitive,romon;
+} else={
+	/system script set [find where name=$scrName] source=$scr dont-require-permissions=no policy=ftp,reboot,read,write,policy,test,password,sniff,sensitive,romon;
+}
+:if ([:len [/system scheduler find where name=$schName]] < 1) do={
+	/system scheduler add disabled=no interval=1m name=$schName on-event="/system script run rpsInit;" policy=ftp,reboot,read,write,policy,test,password,sniff,sensitive,romon start-date=jan/01/1970 start-time=00:00:00;
+} else={
+	/system scheduler set [find where name=$schName] disabled=no interval=1m on-event="/system script run rpsInit;" policy=ftp,reboot,read,write,policy,test,password,sniff,sensitive,romon start-date=jan/01/1970 start-time=00:00:00;
+}
+
+##dont trigger, just exit, the default config makes the device hang until done
+##/system script run rpsInit;
